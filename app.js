ca63609628b494f673a4c4b102aaca32f1847870
Hugh <det.nosnibor00@gmail.com>
2020-06-05 16:29:37 -0500

    Edited the comments to be informative

--- app.js
+++ app.js
@@ -13,11 +13,13 @@ function getPokemon(pokemon) {
   //fetch information for the pokemon
   fetch('https://pokeapi.co/api/v2/pokemon/' + pokemon)
     .then(response => response.json()) // turn the response into json
-    .then(json => { 
+    .then(json => {
+      //create output string and assign the pokemon name to it
       let outputString = json.name + ': '
+      //location where the types array is at in the returned json data
       let pokeType = json.types;
       pokeType.forEach(typeItem => {
-        //push the type to the array where the key is the name of the pokemon
+        //push the type to the array located in the newObj where the key is the name of the pokemon
         typesObj[json.name].push(typeItem.type.name)
 
       });
